8fdab17db1e9677263eff64235bfa5b3d3bf6ce8
elPeiretti <tomaspeiretti@gmail.com>
2023-10-08 16:04:30 -0300

    change repo usage to service usage in TipoUsuarioController

--- src/main/java/dan/ms/tp/msusuarios/rest/controller/TipoUsuarioController.java
+++ src/main/java/dan/ms/tp/msusuarios/rest/controller/TipoUsuarioController.java
@@ -4,23 +4,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import dan.ms.tp.msusuarios.dao.TipoUsuarioJpaRepository;
 import dan.ms.tp.msusuarios.modelo.TipoUsuario;
+import dan.ms.tp.msusuarios.rest.service.TipoUsuarioService;
 
 @RestController
 @RequestMapping("api/tipo-usuario")
 public class TipoUsuarioController {
  
     @Autowired
-    TipoUsuarioJpaRepository tipoUsrRepo;
+    TipoUsuarioService tuService;
 
 
     @GetMapping
     public ResponseEntity<List<TipoUsuario>> getAllTipoUsuario(){
-        return ResponseEntity.ok().body(tipoUsrRepo.findAll());
+        return ResponseEntity.ok().body(tuService.getAllTipoUsuario());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TipoUsuario> getTipoUsuarioById(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok().body(tuService.getTipoUsuario(id));
+        } catch (Exception e) {
+            // TODO: handle exception RecursoNoEncontrado
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/dan/ms/tp/msusuarios/rest/service/TipoUsuarioService.java
+++ src/main/java/dan/ms/tp/msusuarios/rest/service/TipoUsuarioService.java
@@ -1,5 +1,10 @@
 package dan.ms.tp.msusuarios.rest.service;
 
+import java.util.List;
+
+import dan.ms.tp.msusuarios.modelo.TipoUsuario;
+
 public interface TipoUsuarioService {
-  //TODO: define methods
+  List<TipoUsuario> getAllTipoUsuario();
+  TipoUsuario getTipoUsuario(Integer id);
 }
