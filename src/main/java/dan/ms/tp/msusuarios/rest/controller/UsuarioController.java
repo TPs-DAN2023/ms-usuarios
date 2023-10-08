27bdc37b6bbb0856141a867f2a188574c9acbff4
elPeiretti <tomaspeiretti@gmail.com>
2023-10-08 16:00:15 -0300

    change repo usage to service usage

--- src/main/java/dan/ms/tp/msusuarios/rest/controller/UsuarioController.java
+++ src/main/java/dan/ms/tp/msusuarios/rest/controller/UsuarioController.java
@@ -12,28 +12,27 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import dan.ms.tp.msusuarios.dao.UsuarioJpaRepository;
 import dan.ms.tp.msusuarios.modelo.Usuario;
+import dan.ms.tp.msusuarios.rest.service.UsuarioService;
 
 @RestController
 @RequestMapping("api/usuario")
 public class UsuarioController {
     
     @Autowired
-    private UsuarioJpaRepository usuarioRepo;
+    private UsuarioService usuarioService;
 
     @GetMapping
     public ResponseEntity<List<Usuario>> getAllUsuario() {
-        return ResponseEntity.ok().body(usuarioRepo.findAll());
+        return ResponseEntity.ok().body(usuarioService.getAllUsuarios());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> getUsuarioById(@PathVariable Integer id) {
         try {
-            return ResponseEntity.ok().body(usuarioRepo.findById(id).orElseThrow(() -> new Exception()));
+            return ResponseEntity.ok().body(usuarioService.getUsuarioById(id));
         } catch (Exception e) {
-            // TODO: handle exception
+            // TODO: handle exception RecursoNoEncontrado
         }
         return ResponseEntity.notFound().build();
     }
@@ -42,49 +41,36 @@ public class UsuarioController {
     public ResponseEntity<Usuario> getUsuarioByIdCliente(@PathVariable Integer idCliente) {
         
         try {
-
-            return ResponseEntity.ok().body(usuarioRepo.findByIdCliente(idCliente));
+            return ResponseEntity.ok().body(usuarioService.getUsuarioByCliente(idCliente));
         } catch (Exception e) {
-            // TODO: handle exception
+            // TODO: handle exception RecursoNoEncontrado
         }
 
         return ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/{tipoUsuario}")
+    /*ESTA NO LA ENTENDI, dice por tipo de usuario y ciente
+
+    @GetMapping("/{tipoUsuario}/")
     public ResponseEntity<List<Usuario>> getAllUsuarioByTipoUsuario(@PathVariable String tipoUsuario) {
 
-        return ResponseEntity.ok().body(usuarioRepo.findAllByTipoUsuario(tipoUsuario));
+        return ResponseEntity.ok().body(usuarioService.findAllByTipoUsuario(tipoUsuario));
     }
+    */
 
     @PostMapping
     public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuario) {
-        return ResponseEntity.ok().body(usuarioRepo.save(usuario));
+        return ResponseEntity.ok().body(usuarioService.saveOrUpdateUsuario(usuario, null));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Usuario> putUsuario(@RequestBody Usuario nuevoUsuario, @PathVariable Integer id) {
         
-        Usuario usuario = null;
-        try {
-            usuario = usuarioRepo.findById(id).orElseThrow(() -> new Exception());
-            usuario.setUserName(nuevoUsuario.getUserName());
-            usuario.setCliente(nuevoUsuario.getCliente());
-            usuario.setCorreoElectronico(nuevoUsuario.getCorreoElectronico());
-            usuario.setPassword(nuevoUsuario.getPassword());
-            usuario.setTipoUsuario(nuevoUsuario.getTipoUsuario());
-            
-        } catch (Exception e) {
-            nuevoUsuario.setId(id);
-            usuario = nuevoUsuario;
-        }
-
-        return ResponseEntity.ok().body(usuarioRepo.save(usuario));
+        return ResponseEntity.ok().body(usuarioService.saveOrUpdateUsuario(nuevoUsuario, id));
     }
 
     @DeleteMapping("/{id}")
     public void deleteUsuario(@PathVariable Integer id) {
-
-        usuarioRepo.deleteById(id);  //Ver si le agregamos un return codigo 204?
+        usuarioService.deleteUsuario(id);  //Ver si le agregamos un return codigo 204?
     }
 }
--- src/main/java/dan/ms/tp/msusuarios/rest/service/UsuarioService.java
+++ src/main/java/dan/ms/tp/msusuarios/rest/service/UsuarioService.java
@@ -1,5 +1,12 @@
 package dan.ms.tp.msusuarios.rest.service;
 
+import java.util.List;
+import dan.ms.tp.msusuarios.modelo.Usuario;
+
 public interface UsuarioService {
-  //TODO: define methods
+  List<Usuario> getAllUsuarios();
+  Usuario getUsuarioById(Integer id);
+  Usuario getUsuarioByCliente(Integer idCliente);
+  Usuario saveOrUpdateUsuario(Usuario nuevoUsuario, Integer id);
+  void deleteUsuario(Integer id);
 }
