eadf6f8517b4b9bce37ff03f0dcf31ce4deaf878
ChortJulio <chortjulio@gmail.com>
2024-02-27 10:50:58 -0300

    Remove CORS policy (hate you CORS!)

--- src/main/java/dan/ms/tp/msusuarios/MsUsuariosApplication.java
+++ src/main/java/dan/ms/tp/msusuarios/MsUsuariosApplication.java
@@ -2,28 +2,12 @@ package dan.ms.tp.msusuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MsUsuariosApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MsUsuariosApplication.class, args);
-  }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-            .allowedOrigins("*")
-            .allowedHeaders("*");
-      }
-    };
   }
 
 }
